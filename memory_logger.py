19523052c99478122e280d68b35ecb5d19767ad4
Sarmitha <156922837+sarmi2325@users.noreply.github.com>
2025-07-07 16:08:40 +0530

    Delete memory_logger.py

--- memory_logger.py
+++
@@ -1,8 +0,0 @@
-import psutil
-import os
-
-def log_memory(stage=""):
-    process = psutil.Process(os.getpid())
-    mem = process.memory_info().rss / (1024 * 1024)
-    print(f"[MEMORY] {stage}: {mem:.2f} MB")
-
