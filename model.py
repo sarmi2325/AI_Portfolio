654c6fe1108c903f5e3b52598db12dfb81eee3f7
Sarmitha <156922837+sarmi2325@users.noreply.github.com>
2025-07-09 02:03:48 +0530

    Delete model.py

--- model.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Like(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    count = db.Column(db.Integer, default=0)
-
-
